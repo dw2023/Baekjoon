b71458bd0263b309da2de4d24219f4ee86dbf83d
dw2023 <dwkim126@gmail.com>
2023-03-15 18:04:20 +0900

    10998번

---
+++ src/main/java/com/baekjoon/step1/q10998/Main.java
@@ -0,0 +1,18 @@
+package com.baekjoon.step1.q10998;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] nums = sc.nextLine().split(" ");
+
+        int n1 = Integer.parseInt(nums[0]);
+        int n2 = Integer.parseInt(nums[1]);
+
+        System.out.println(n1 * n2);
+
+        sc.close();
+    }
+}
